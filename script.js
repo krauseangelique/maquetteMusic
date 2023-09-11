1ecbf2010547aafe004f23a8a3c15ec1a556ad76
angelique KRAUSE <krause_angelique@hotmail.com>
2023-09-11 11:18:22 +0200

    folder structure and hack JQuery to add the same header on each pages

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log("Hello");
 
 /* hack JS on JQuery to have the same header and footer on all yours pages */
 $(function(){
-    $("#header").load("header.html"); 
-    $("#footer").load("footer.html"); 
+    $("#header").load("../header.html"); 
+    
+    $("#footer").load("../footer.html"); 
   });
